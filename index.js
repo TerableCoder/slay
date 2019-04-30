86862a3c8e2d8c965f071aa509ec67a9eb533e5f
Tera Coder <TerableCoder@gmail.com>
2019-04-30 16:17:02 -0400

    remove S_LOGIN

--- index.js
+++ index.js
@@ -1,52 +1,45 @@
-module.exports = function Slay(mod) {
-	const {command} = mod.require
+module.exports = function drop(mod) {
+	mod.game.initialize(["me"]);
 
-	let gameId = -1n,
-		isCastanic = false,
-		location = null,
+	let location = null,
 		locRealTime = 0,
-		curHp = 0,
-		maxHp = 0
-
-	mod.hook('S_LOGIN', mod.patchVersion < 81 ? 12 : 13, event => {
-		({gameId} = event)
-		isCastanic = Math.floor((event.templateId - 10101) / 200) === 3
-	})
+		curHp = 0n,
+		maxHp = 0n;
 
 	mod.hook('S_PLAYER_STAT_UPDATE', 10, event => {
-		curHp = Number(event.hp)
-		maxHp = Number(event.maxHp)
-	})
+		curHp = event.hp;
+		maxHp = event.maxHp;
+	});
 
 	mod.hook('S_CREATURE_CHANGE_HP', 6, event => {
-		if(event.target === gameId) {
-			curHp = Number(event.curHp)
-			maxHp = Number(event.maxHp)
+		if({mod.game.me.is(event.target)){
+			curHp = event.curHp;
+			maxHp = event.maxHp;
 		}
-	})
+	});
 
 	mod.hook('C_PLAYER_LOCATION', 5, event => {
-		location = event
-		locRealTime = Date.now()
-	})
+		location = event;
+		locRealTime = Date.now();
+	});
 
-	command.add(['drop', 'slay', 'slaying'], percent => {
-		percent = Number(percent)
+	mod.command.add(['drop', 'slay', 'slaying'], percent => {
+		percent = Number(percent);
 
-		if(!(percent > 0 && percent <= 100) || !curHp) return
+		if(!(percent > 0 && percent <= 100) || !curHp) return;
 
-		const percentToDrop = curHp * 100 / maxHp - percent
+		const percentToDrop = Number(curHp * 100n / maxHp) - percent;
 
-		if(percentToDrop <= 0) return
+		if(percentToDrop <= 0) return;
 
 		mod.send('C_PLAYER_LOCATION', 5, Object.assign({}, location, {
-			loc: location.loc.addN({z: 400 + percentToDrop*(isCastanic ? 20 : 10)}),
+			loc: location.loc.addN({z: 400 + percentToDrop*(mod.game.me.race =='castanic' ? 20 : 10)}),
 			type: 2,
 			time: location.time - locRealTime + Date.now() - 50
-		}))
+		}));
 		mod.send('C_PLAYER_LOCATION', 5, Object.assign(location, {
 			type: 7,
 			time: location.time - locRealTime + Date.now() + 50
-		}))
-	})
+		}));
+	});
 }
